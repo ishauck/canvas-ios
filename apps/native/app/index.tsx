42be3abf56ec74b1a3673c5597609f9993e554cf
ishauck <98329201+ishauck@users.noreply.github.com>
2025-06-05 14:11:45 -0700

    Secondary Commit

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,13 +1,12 @@
 import { Stack, Link } from 'expo-router';
 
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+import { Button } from '@/components/Button';
+import { Container } from '@/components/Container';
+import { ScreenContent } from '@/components/ScreenContent';
 
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Home' }} />
       <Container>
         <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
         <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
