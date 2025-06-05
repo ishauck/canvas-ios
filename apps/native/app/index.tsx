352358b770216fa0233d254de75f7fdf1b1a2473
ishauck <98329201+ishauck@users.noreply.github.com>
2025-06-05 14:16:10 -0700

    Refactor Home component in native app to simplify JSX structure by removing unnecessary fragments.

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,4 +1,4 @@
-import { Stack, Link } from 'expo-router';
+import { Link } from 'expo-router';
 
 import { Button } from '@/components/Button';
 import { Container } from '@/components/Container';
@@ -6,13 +6,11 @@ import { ScreenContent } from '@/components/ScreenContent';
 
 export default function Home() {
   return (
-    <>
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </>
+    <Container>
+      <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
+      <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
+        <Button title="Show Details" />
+      </Link>
+    </Container>
   );
 }
